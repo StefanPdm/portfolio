6c4dc9cb1ed76417384f512fb1b0ce577974645d
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2023-04-10 17:06:34 +0200

    final 1.2.1

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,20 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+   selector: "app-footer",
+   templateUrl: "./footer.component.html",
+   styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent {
-  hover() {
-    document.getElementById('i-point-footer')?.setAttribute('width', '249px');
-  }
+   hover() {
+      document.getElementById("i-point-footer")?.setAttribute("width", "249px");
+   }
 
-  dehover() {
-    document.getElementById('i-point-footer')?.setAttribute('width', '7.42px');
-  }
+   dehover() {
+      document.getElementById("i-point-footer")?.setAttribute("width", "7.42px");
+   }
 
-  toggleImpressum() {
-    document.getElementById('impressum').classList.toggle('show-impressum');
-  }
+   toggleImpressum() {
+      document.getElementById("impressum").classList.toggle("show-impressum");
+   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+   selector: "app-header",
+   templateUrl: "./header.component.html",
+   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-  hover() {
-    document.getElementById('i-point')?.setAttribute('width', '249px');
-  }
+   hover() {
+      document.getElementById("i-point")?.setAttribute("width", "249px");
+      document.getElementById("i-point")?.setAttribute("height", "7px");
+   }
 
-  dehover() {
-    document.getElementById('i-point')?.setAttribute('width', '7.42px');
-  }
+   dehover() {
+      document.getElementById("i-point")?.setAttribute("width", "7.42px");
+      document.getElementById("i-point")?.setAttribute("height", "8.14px");
+   }
 
-  toggleMobileMenu(id: string) {
-    document.getElementById('mobile-menu').classList.toggle('toggle-menu');
-    document.getElementById(id).classList.toggle('d-none');
-  }
+   toggleMobileMenu(id: string) {
+      document.getElementById("mobile-menu").classList.toggle("toggle-menu");
+      document.getElementById(id).classList.toggle("d-none");
+   }
 }
