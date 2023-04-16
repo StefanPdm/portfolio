8480f784b7032764d3ff20ae7408dd7ec3d2e19e
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2023-04-16 22:48:30 +0200

    turning icons for the skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+  mouse = {
+    x: 0,
+    y: 0,
+  };
 
+  ngOnInit() {
+    const skillsContainer = document.getElementById('skills');
+    const root = document.documentElement;
+    skillsContainer.addEventListener('mousemove', (e) => {
+      root.style.setProperty('--mouse-x', e.x + 'px');
+      root.style.setProperty('--mouse-y', e.y + 'px');
+    });
+  }
 }
