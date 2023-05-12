7362dca1eb7970398e7da2c7f102a4264696ec96
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2023-05-12 20:10:50 +0200

    svg footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideDatabase, getDatabase } from '@angular/fire/database';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { environment } from '../environments/environment';
+// import { provideAuth, getAuth } from '@angular/fire/auth';
+// import { provideDatabase, getDatabase } from '@angular/fire/database';
+// import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { HeaderComponent } from './header/header.component';
 import { StartComponent } from './start/start.component';
 import { FooterComponent } from './footer/footer.component';
@@ -36,9 +36,9 @@ import { ImpressumComponent } from './impressum/impressum.component';
     FormsModule,
     ReactiveFormsModule,
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
+    // provideAuth(() => getAuth()),
+    // provideDatabase(() => getDatabase()),
+    // provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
