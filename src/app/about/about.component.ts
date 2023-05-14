ae2581788eb08f1ee4eafbab995dfa9117fff582
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2023-05-14 14:11:48 +0200

    movement second portrait

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
   @Input() language: string;
+  hiddenElement: any;
+  observer: IntersectionObserver;
+
+  ngOnInit() {
+    this.hiddenElement = document.querySelectorAll('.hidden');
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log('observers', entry.isIntersecting, entry.target);
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+    this.hiddenElement.forEach((element) => {
+      this.observer.observe(element);
+    });
+  }
 }
